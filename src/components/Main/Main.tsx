09c915bd6a19816dc2acf477bf4bbd11220db9dc
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-14 01:12:52 -0400

    Restyle home page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,46 +1,52 @@
 import image from "../../../public/assets/intro-img.png";
+import ComputerCanvas from "../canvas/Computers";
+import { globalStyles } from "../styles";
 import { styles } from "./MainStyles";
+import { motion } from "framer-motion";
 import React from "react";
 import TypeIt from "typeit-react";
 
 const Main = () => {
   return (
-    <div id="Home" className={styles.mainContainer}>
-      <div className="hidden sm:flex justify-center items-center w-full h-[100%]">
-        <img
-          className="w-[600px] max-h-[100vmin] md:w-[700px] lg:w-[800px] xl:w-[900px] ease-out duration-100 z-0"
-          src={image.src}
-          alt="laptop-illustration"
-        />
-      </div>
+    <section id="Home" className={styles.mainContainer}>
+      <div
+        className={`${globalStyles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div
+            className={`w-1 h-80 bg-gradient-to-b from-indigo-700 to-transparent sm:h-40`}
+          />
+        </div>
+        <div>
+          <h1 className={`${styles.mainText} flex flex-col items-start`}>
+            Hi, I'm <br className="block md:hidden" />
+            <span className="text-[#915eff]">Esther SH Choi</span>
+            <div className="text-3xl text-[#915eff]">
+              <TypeIt
+                cursorchar={<div>*</div>}
+                options={{ loop: true }}
+                getBeforeInit={(instance) => {
+                  instance
+                    .type("I am a full-stack developer.")
+                    .pause(1200)
+                    .delete(28)
+                    .pause(500)
+                    .type("I make imagination come to life!")
+                    .pause(1200);
 
-      <div className="absolute top-0 w-full h-[100%] p-5">
-        <div className="flex flex-col justify-center items-center h-[100%]">
-          <div className="flex flex-col justify-center items-center h-[100px]">
-            <h2>Hi, </h2>
-            <h2>I'm Esther Choi.</h2>
-          </div>
-          <div className="text-2xl">
-            <TypeIt
-              cursorchar={<div className="text-2xl">|</div>}
-              options={{ loop: true }}
-              getBeforeInit={(instance) => {
-                instance
-                  .type("I am a full-stack developer.")
-                  .pause(1200)
-                  .delete(28)
-                  .pause(500)
-                  .type("I make imagination come to life!")
-                  .pause(1200);
-
-                return instance;
-              }}
-            />
-          </div>
-          <button className={styles.resumeButton}>Resume</button>
+                  return instance;
+                }}
+              />
+            </div>
+          </h1>
+          {/* <p className={`${styles.subText} mt-2 text-white-100`}>
+            I develop user interfaces, database, and web applications.
+          </p> */}
         </div>
       </div>
-    </div>
+      <ComputerCanvas />
+    </section>
   );
 };
 
--- src/components/Main/MainStyles.ts
+++ src/components/Main/MainStyles.ts
@@ -2,4 +2,13 @@ export const styles = {
   mainContainer: "relative w-full h-screen text-center pt-20",
   resumeButton:
     "shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-[#5651e5] to-[#709dff] text-white mt-10 px-5 py-2",
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-16 py-10",
+
+  mainText:
+    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+  subText:
+    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+  mainColor: "#915eff",
 };
