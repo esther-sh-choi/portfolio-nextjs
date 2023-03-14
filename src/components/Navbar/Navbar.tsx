0954f75e9842e9700f56d4a91b9c1747e3448ab0
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-13 23:40:42 -0400

    Dynamic navbar background:

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,12 +4,27 @@ import { routes } from "../routes";
 import { styles } from "./NavbarStyles";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [shadow, setShadow] = useState(false);
+  const [navBackground, setNavBackground] = useState("#ecf0f3");
+  const [linkColor, setLinkColor] = useState("#1f2937");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.asPath === "/projects/food-pickup") {
+      setNavBackground("transparent");
+      setLinkColor("#ecf0f3");
+    } else {
+      setNavBackground("#ecf0f3");
+      setLinkColor("#1f2937");
+    }
+  }, [router]);
 
   useEffect(() => {
     const handleShadow = () => {
@@ -27,11 +42,17 @@ const Navbar = () => {
   };
 
   return (
-    <div className={`${styles.navbarContainer} ${shadow ? "shadow-lg" : ""}`}>
+    <div
+      className={`${styles.navbarContainer} ${shadow ? "shadow-lg" : ""}`}
+      style={{ backgroundColor: `${navBackground}` }}
+    >
       <div className={styles.navbarContent}>
         <Image src={logo} alt="/" width="70" height="70" />
         <div>
-          <ul className={styles.navbarLinkContainer}>
+          <ul
+            className={styles.navbarLinkContainer}
+            style={{ color: `${linkColor}` }}
+          >
             {routes.map((route) => (
               <Link href={route.path} key={route.id}>
                 <li className={styles.navbarLink}>{route.name}</li>
---
+++ src/pages/projects/food-pickup.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FoodPickup = () => {
+  return <div>food-pickup</div>;
+};
+
+export default FoodPickup;
