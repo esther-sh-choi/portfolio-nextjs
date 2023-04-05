5a06d1a119cb339a8467e7cb25b0cb0e79797a67
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-04-05 03:06:06 -0400

    Skills section

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -5,18 +5,17 @@ import React from "react";
 const Skills = () => {
   return (
     <div id="skills" className="h-screen">
-      <div className="max-w-[1240px] m-10 sm:m-20 lg:m-30 xl:mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text=[#5651e5]">
-          Skills
-        </p>
-        <h2 className="py-4">What I Can Do</h2>
-        <div className="grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+      {/* <div className="max-w-[1240px] m-10 sm:m-20 lg:m-30 xl:mx-auto flex flex-col justify-center h-full"> */}
+      <div className="max-w-[1240px] ml-10 flex flex-col justify-center w-[50%] h-full">
+        <p className="text-xl tracking-widest uppercase text-white">Skills</p>
+        <h2 className="py-4">Here's what I can do</h2>
+        <div className="p-4 rounded-xl grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-8 bg-[#ffae42] shadow-xl">
           {skills.map((skill) => (
             <div
               key={skill.id}
-              className="p-4 shadow-xl rounded-xl hover:scale-105 ease-in duration-200"
+              className="p-4 shadow-xl rounded-xl hover:scale-105 ease-in duration-200 bg-white"
             >
-              <div className="grid grid=cols-2 gap-4 justify-center items-center">
+              <div className="grid grid=cols-2 gap-4 justify-center items-center ">
                 <div className="m-auto">
                   <Image
                     id={skill.id}
