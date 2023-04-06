8f149620f6bea36288e19b61ca0f5731920303a0
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-04-06 16:21:17 -0400

    fix contact form on mobile

--- src/components/Contact/ContactStyles.ts
+++ src/components/Contact/ContactStyles.ts
@@ -1,6 +1,6 @@
 export const styles = {
   sendButton:
-    "shadow-md shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-red-300 via-rose-400 to-red-300 text-white mt-5 px-5 py-2 md:w-40",
+    "shadow-md shadow-gray-400 rounded-xl uppercase bg-rose-400 text-white mt-5 px-5 py-2 md:w-40",
   input:
-    "block py-2.5 px-0 w-full text-xs sm:text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-rose-300 focus:outline-none focus:ring-0 focus:border-rose-300 peer",
+    "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent rounded-none border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-rose-300 focus:outline-none focus:ring-0 focus:border-rose-300 peer",
 };
