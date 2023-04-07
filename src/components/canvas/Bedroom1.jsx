7452c3f1a65fb01156f38fbf755ae460bec6b074
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-04-07 12:27:14 -0400

    revert to yesterday

--- src/components/canvas/Bedroom1.jsx
+++ src/components/canvas/Bedroom1.jsx
@@ -30,8 +30,8 @@ const Bedroom = ({ isSmallScreen }) => {
             trigger: "#home",
             // pin: "#home",
             pinSpacing: false,
-            start: "top bottom",
-            end: "top top",
+            start: "top 99%",
+            end: "top 1%",
             scrub: true,
             immediateRender: false,
             snap: !isSmallScreen && {
@@ -39,7 +39,6 @@ const Bedroom = ({ isSmallScreen }) => {
               duration: { min: 0.2, max: 0.5 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
               delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
               ease: "power1", // the ease of the snap animation ("power3" by default)
-              directional: false,
             },
           },
         })
@@ -53,8 +52,8 @@ const Bedroom = ({ isSmallScreen }) => {
           z: 0.01601712490957726,
           scrollTrigger: {
             trigger: "#about",
-            start: "top bottom",
-            end: "top top",
+            start: "top 99%",
+            end: "top 1%",
             scrub: true,
             // pin: "#about",
             pinSpacing: false,
@@ -64,7 +63,6 @@ const Bedroom = ({ isSmallScreen }) => {
               duration: { min: 0.2, max: 0.5 },
               delay: 0.2,
               ease: "power1",
-              directional: false,
             },
           },
         })
@@ -78,8 +76,8 @@ const Bedroom = ({ isSmallScreen }) => {
           z: 0.0005459788201534593,
           scrollTrigger: {
             trigger: "#skills",
-            start: "top 99%",
-            end: "top top",
+            start: "top bottom",
+            end: "top 1%",
             // pin: "#skills",
             pinSpacing: false,
             scrub: true,
@@ -89,21 +87,19 @@ const Bedroom = ({ isSmallScreen }) => {
               duration: { min: 0.2, max: 0.5 },
               delay: 0.2,
               ease: "power1",
-              directional: false,
             },
           },
         })
       : tl.pause();
-
     // Computer - Projects
     !isOpen
       ? tl.to(camera.position, {
-          x: -0.547425441128117,
-          y: 0.082282798237053556,
-          z: -0.0075648388487006014,
+          x: -0.02947425441128117,
+          y: 0.0019282798237053556,
+          z: 0.0015648388487006014,
           scrollTrigger: {
             trigger: "#projects",
-            start: "top bottom",
+            start: "top 99%",
             end: "top 1%",
             // markers: true,
             // pin: "#projects",
@@ -115,22 +111,20 @@ const Bedroom = ({ isSmallScreen }) => {
               duration: { min: 0.2, max: 0.5 },
               delay: 0.2,
               ease: "power1",
-              directional: false,
             },
           },
         })
       : tl.pause();
-
     // Coffee - contact me
     !isOpen
       ? tl.to(camera.position, {
-          x: isSmallScreen ? 0.2 : -0.026012678122319,
-          y: isSmallScreen ? 0.8 : 0.06005036637699721,
-          z: isSmallScreen ? 3 : 0.04399400225061104,
+          x: isSmallScreen ? -0.01 : -0.026012678122319,
+          y: isSmallScreen ? 0.05 : 0.06005036637699721,
+          z: isSmallScreen ? 0.02 : 0.04399400225061104,
           scrollTrigger: {
             trigger: "#contact",
-            start: "top bottom",
-            end: "top 1%",
+            start: "top 99%",
+            end: "top 1px",
             // pin: "#contact",
             pinSpacing: false,
             scrub: true,
@@ -141,7 +135,6 @@ const Bedroom = ({ isSmallScreen }) => {
               duration: { min: 0.2, max: 0.5 },
               delay: 0.2,
               ease: "power1",
-              directional: false,
             },
           },
         })
