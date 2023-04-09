4e7c1057ce156f2fca9403b3e6445433d8904449
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-04-08 20:24:21 -0400

    Add toggleAction

--- src/components/canvas/Bedroom1.jsx
+++ src/components/canvas/Bedroom1.jsx
@@ -19,6 +19,10 @@ const Bedroom = ({ isSmallScreen }) => {
     ignoreMobileResize: true,
   });
 
+  ScrollTrigger.defaults({
+    toggleActions: "restart pause resume none",
+  });
+
   useLayoutEffect(() => {
     !isOpen
       ? tl.to(camera.position, {
